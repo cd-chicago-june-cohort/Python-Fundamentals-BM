4fedfacea42df702dc503df814c2da88d0cdb3cd
Bald Mike <baldmike312@gmail.com>
2017-07-03 14:18:04 -0500

    added multsumave

---
+++ multsumave.py
@@ -0,0 +1,19 @@
+
+# Multiples
+for x in range (1, 1000):
+    if (x % 2 == 1):
+        print x
+
+for y in range (5, 1000001):
+    if (y % 5 == 0):
+        print y
+
+
+# Sum List
+a = [1,2,5,10,255,3]
+
+numsum = sum(a)
+print numsum
+
+ave = numsum/len(a)
+print ave
