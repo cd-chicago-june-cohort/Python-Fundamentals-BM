5ddc1913e7e3ca3e4e70706fe1fb03be139a762e
Bald Mike <baldmike312@gmail.com>
2017-07-04 17:14:41 -0500

    re-push dict

--- dictionary_basics.py
+++ dictionary_basics.py
@@ -1,1 +1,11 @@
-about = ['Bald Mike' ] 
+about = {'name': 'Bald Mike', 'age': 12, 'country of birth': 'The United States', 'favorite language': 'Python'}
+
+def print_info (arr):
+
+    print 'My name is', arr['name']
+    print 'My age is', arr['age']
+    print 'My country of birth is', arr['country of birth']
+    print 'My favorite language is', arr['favorite language']
+
+
+print_info(about)
