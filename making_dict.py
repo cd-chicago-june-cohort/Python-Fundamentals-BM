fd059b39b1119f3a0003862ff404591a6a5edc3e
Bald Mike <baldmike312@gmail.com>
2017-07-04 20:58:46 -0500

    hacker challenge

--- making_dict.py
+++ making_dict.py
@@ -1,12 +1,20 @@
 name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
-favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas", "chickens"]
 
 def make_dict(arr1, arr2):
     new_dict = {}
 
-    for i in range (0, len(arr1)):
-        new_dict[arr1[i]] = arr2[i]
+    if (len(arr1) >= len(arr2)):
+
+        for i in range (0, len(arr1)):
+            new_dict[arr1[i]] = arr2[i]
+    
+    else:
+        
+        for i in range (0, len(arr1)):
+            new_dict[arr2[i]] = arr1[i]
 
     print new_dict
 
 make_dict(name, favorite_animal)
+
