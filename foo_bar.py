7ab0bc55a33a0bbacbe847f5d013c4e1a0074836
Bald Mike <baldmike312@gmail.com>
2017-07-04 22:24:01 -0500

    foo_bar

---
+++ foo_bar.py
@@ -0,0 +1,23 @@
+def foo_bar ():
+
+    x = 10
+    
+
+    for i in range (100, 100001):
+        prime = True
+
+        for k in range (i/2, 1, -1):
+            if (i%k == 0):
+                prime = False
+            
+        if (prime == True):
+            print i, 'foo'
+
+        if ((i/x)==x):
+            print i, 'bar (of ', x, ')'
+            x += 1
+        
+        else: print i 
+            
+foo_bar()
+
